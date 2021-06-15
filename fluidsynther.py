6cf886ff1f20253f53ffad8d5587ef609dd48f7a
GermanAngel <j.gruenler@gmx.de>
2021-06-15 17:16:17 +0200

    fluidsynth for each os

--- fluidsynther.py
+++ fluidsynther.py
@@ -1,8 +1,22 @@
 import fluidsynth
+import sys
 
 def initFluidSynth():
     fs = fluidsynth.Synth(1)
-    fs.start(driver = 'portaudio')
+    platform = sys.platform
+    print(platform)
+    # https://www.fluidsynth.org/api/CreatingAudioDriver.html
+    if 'linux' in platform:
+        this_driver = 'alsa'
+    elif 'darwin' in platform:
+        this_driver = 'portaudio'
+    elif 'freebsd' in platform:
+        this_driver = 'oss'
+    else:
+        # windows
+        this_driver = 'dsound'
+    print(this_driver)
+    fs.start(driver = this_driver)
     sfid = fs.sfload("default-GM.sf2") 
     fs.program_select(0, sfid, 0, 0)
     return fs
